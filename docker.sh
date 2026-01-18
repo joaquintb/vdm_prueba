b8f880768a1944498571b0490c0cd7658e1bb601
Joaquin Torres <joaquintobrw@gmail.com>
2026-01-18 20:49:49 +0100

    First docker version

---
+++ docker.sh
@@ -0,0 +1,15 @@
+docker build -f docker\Dockerfile -t vdm-pipeline .
+
+docker run --rm `
+  -v ${PWD}\results:/app/results `
+  -v ${PWD}\data:/app/data `
+  vdm-pipeline
+
+docker images | findstr vdm-pipeline
+
+
+docker run --rm vdm-pipeline python -c "import torch; print(torch.__version__); print('cuda:', torch.cuda.is_available())"
+
+docker run --rm `
+  -v ${PWD}:/app `
+  vdm-pipeline
